b1f0c30f17fce719881ffbdbc2fb7ffcfb140216
Amimu10 <amimul211@gmail.com>
2023-11-05 14:08:40 +0600

    update all books & show all books there

--- src/component/AllBooks.jsx
+++ src/component/AllBooks.jsx
@@ -2,15 +2,14 @@ import { useLoaderData } from "react-router-dom";
 import BooksCard from "./BooksCard";
 
 const AllBooks = () => {
-    const books = useLoaderData(); 
-
-
+    const books = useLoaderData();
     return (
-        <div>
-            all books here
-            {
-                books.map(book=> <BooksCard key={book._id} books={books}></BooksCard>)
+        <div>  
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mx-auto max-w-[1200px] my-12">
+          {
+                books.map(book => <BooksCard key={book._id} book={book}></BooksCard>)
             }
+          </div>
         </div>
     );
 };
--- src/component/BooksCard.jsx
+++ src/component/BooksCard.jsx
@@ -1,21 +1,23 @@
-
-
-const BooksCard = ({books}) => {
-    const { image,  
-        name, 
-        quantity, 
-        category,  
-        author, 
-        rating,
-        description} = books; 
-    
+const BooksCard = ({ book }) => {
+    const { image, name, category, author, rating } = book;
+  
     return (
-        <div>
-            {
-                books.length 
-            }
+      <div className="card card-compact font-inter shadow-xl">
+        <figure>
+          <img className="h-[240px] w-full" src={image} alt="Book Cover" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{name}</h2>
+          <p className="font-inter">Category: {category}</p>
+          <p className="font-inter">Author: {author}</p>
+          <p className="font-inter">Rating: {rating}</p>
+          <div className="card-actions justify-end">
+            <button className="bg-[#2B3441] px-3 py-2 text-white rounded-md">Update</button>
+          </div>
         </div>
+      </div>
     );
-};
-
-export default BooksCard;
+  };
+  
+  export default BooksCard;
+  
