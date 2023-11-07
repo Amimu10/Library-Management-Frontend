981cd73513127b8df55a1e2932666b7d8ee75722
Amimu10 <amimul211@gmail.com>
2023-11-08 02:31:07 +0600

    update 404 page

--- src/component/ErrorPage.jsx
+++ src/component/ErrorPage.jsx
@@ -1,8 +1,12 @@
-
+import image from "../assets/404.png"
+import Navbar from "./Navbar";
 const ErrorPage = () => {
     return (
-        <div>
-            <h3>This is error page</h3>
+        <div className="text-center px-6 my-8"> 
+            <Navbar></Navbar>
+            <p className="text-lg font-inter font-medium text-orange-600 ">Page not found</p> 
+            <img src={image} alt="" className="mx-auto"/>
+
         </div>
     );
 };
